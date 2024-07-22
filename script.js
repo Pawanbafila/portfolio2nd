0e8c6ae010270bbf09d3b900e3800a82ab159578
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-07-22 22:05:17 +0530

    add new portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+/*THIS IS FOR LOADING JS*/
+
+// var loader = document.querySelector(".loader-parent");
+// var section = document.querySelector("section");
+
+// function pop1() {
+//   loader.style.display = "none";
+//   section.style.display = "block";
+// }
+
+// function pop() {
+//   loader.style.display = "block";
+//   section.style.display = "none";
+//   setTimeout(pop1, 1000); // Corrected: Pass the function reference without parentheses
+// }
+
+/*HERE OUR GSAP JS START*/
