dbcc5bec947176f1da41df2458817d61f683d0be
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-07-30 18:49:25 +0530

    remove x

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+function openLightbox(img) {
+  var lightbox = document.getElementById("lightbox");
+  var lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "block";
+  lightboxImg.src = img.src;
+}
+
+function closeLightbox() {
+  var lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
+}
+
+
+
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
