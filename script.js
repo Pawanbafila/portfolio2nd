c1aaf65590c4542570140d0e42be561ee9d6208e
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-07-30 15:29:55 +0530

    added imgs

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function openLightbox(img) {
-    var lightbox = document.getElementById("lightbox");
-    var lightboxImg = document.getElementById("lightbox-img");
-    lightbox.style.display = "block";
-    lightboxImg.src = img.src;
-}
-
-function closeLightbox() {
-    var lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "none";
-}
-
-
-
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
